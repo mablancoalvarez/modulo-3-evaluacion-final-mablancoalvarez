851e612288701aa90f77a6b6a3f677bd80fd1cae
mablancoalvarez <mablancoalvarez@gmail.com>
2020-04-23 18:12:07 +0200

    add icons

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import alive from '../images/alive.png';
+import rip from '../images/rip.png';
+
 const CharacterDetails = (props) => {
 console.log(props)
 
@@ -11,8 +14,8 @@ console.log(props)
                 <h2>{props.charactObj.name}</h2>
                 <ul>
                 <li>{props.charactObj.species}</li>
-                <li>{props.charactObj.status}</li>
-                <li>{props.charactObj.origin.name}</li>
+                <li>{props.charactObj.status}<img className="icons" src={props.charactObj.status === 'Alive' ? alive :rip}></img></li>
+                <li>{props.charactObj.origin.name} </li>
                 <li> Episode: {props.charactObj.episode.length}</li>
                 </ul>
             </div>
--- src/components/icons.js
+++ src/components/icons.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import alive from '../images/alive'
+import alive from '../images/alive.png';
+import rip from '../images/rip.png';
 
-const icons = ()=>{
-
-
+const Icons = ()=>{
     return (
         <div>
-        <img src = {alive}/>
+        <img src ={alive}/>
+        <img src = {rip}/>
         </div>
     )
 }
 
-export default icons
+export default Icons
